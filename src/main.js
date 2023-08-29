5b6821a9bb13ce100f753c3cf678ff1c37e1b546
RayandraRhezapaleva <rdra6569@gmail.com>
2023-08-29 20:03:35 +0700

    darkModde

--- src/main.js
+++ src/main.js
@@ -80,3 +80,26 @@ const scrollHeader = () => {
   }
 };
 window.addEventListener("scroll", scrollHeader);
+
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+themeBtn.addEventListener("click", (e) => {
+  if (localStorage.getItem("mode") == "light") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
+
+function darkMode() {
+  html.classList.add("dark");
+  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+  localStorage.setItem("mode", "dark");
+}
+
+function lightMode() {
+  html.classList.remove("dark");
+  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+  localStorage.setItem("mode", "light");
+}
